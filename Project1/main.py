8a664e1a2196bb4aac113a99586510047c494633
lcantugomez <luiscantugomez@outlook.com>
2023-03-01 00:05:08 -0600

    modified the pde grid gen to include coeffs and begin calculating gauss seidel

--- Project1/main.py
+++ Project1/main.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from algebraic_grid import create_alg_grid
-from pde_grid import coeff_calc
+from pde_grid import coeff_calc, pde_grid
 
 y_lower = lambda x: np.piecewise(x,[x<=2,(x>2)*(x<3),x>=3],[lambda t:0, lambda t: (0.15*np.sin((t-2)*np.pi)), lambda t: 0])
 y_upper = lambda x: np.piecewise(x,[x<=2,(x>2)*(x<3),x>=3],[lambda t:1, lambda t: 1 - (0.15*np.sin((t-2)*np.pi)), lambda t: 1])
@@ -17,6 +17,6 @@ x_pts,y_pts = create_alg_grid(i_max,j_max,x_left,x_right,y_lower,y_upper)
 
 
 
-alpha_arr, beta_arr, gamma_arr = coeff_calc(i_max,j_max,x_pts,y_pts,d_exi,d_eta)
-print(len(alpha_arr))
-print((i_max-1)*(j_max-1))
+alpha_mat, beta_mat, gamma_mat = coeff_calc(i_max,j_max,x_pts,y_pts,d_exi,d_eta)
+
+pde_grid(alpha_mat,beta_mat,gamma_mat,x_pts,y_pts,d_exi,d_eta,10**-7,i_max,j_max)
--- Project1/pde_grid.py
+++ Project1/pde_grid.py
@@ -1,24 +1,63 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def coeff_calc(i_max,j_max,x_mat,y_mat,d_exi,d_eta):
     # Initialize alpha, beta, gamma arrays
 
-    alpha_arr = []
-    beta_arr = []
-    gamma_arr = []
+    alpha_mat = np.zeros((i_max-1,j_max-1))
+    beta_mat = np.zeros((i_max-1,j_max-1))
+    gamma_mat = np.zeros((i_max-1,j_max-1))
 
-    for j in range(1,j_max):
+    for j in range(1,j_max):            # Loop only through the interior nodes
         for i in range(1,i_max):
-            # Conditional to only calculate for interior nodes
                 
 
             alpha  = (1/(4*(d_eta**2))) * ( ((x_mat[i,j+1] - x_mat[i,j-1])**2) + ((y_mat[i,j+1] - y_mat[i,j-1]**2)) )
             beta = (1/(4*(d_eta*d_exi))) * ( ((x_mat[i+1,j] - x_mat[i-1,j]) * (x_mat[i,j+1] - x_mat[i,j-1])) + ((y_mat[i+1,j] - y_mat[i-1,j]) * (y_mat[i,j+1] - y_mat[i,j-1])) )
             gamma = (1/(4*(d_exi**2))) * ( ((x_mat[i+1,j] - x_mat[i-1,j])**2) + ((y_mat[i+1,j] - y_mat[i-1,j]**2)) )
 
-            alpha_arr.append(alpha)
-            beta_arr.append(beta)
-            gamma_arr.append(gamma)
-    return alpha_arr, beta_arr, gamma_arr
 
+            alpha_mat[i-1,j-1] = alpha
+            beta_mat[i-1,j-1] = beta
+            gamma_mat[i-1,j-1] = gamma
+
+    return alpha_mat, beta_mat, gamma_mat
+
+
+def pde_grid(alpha_mat,beta_mat,gamma_mat,x_mat,y_mat,d_exi,d_eta,tol,i_max,j_max):
+
+    diff = 1
+    while diff >= tol:
+        for j in range(0,j_max+1):
+            for i in range(0,i_max+1):
+                if i == 0:
+                    if j == 0:
+                        pass
+                    elif j == j_max:
+                        pass
+                    else:
+                        x_mat[i,j] = x_mat[i+1,j]
+                elif i == i_max:
+                    if j == 0:
+                        pass
+                    elif j == j_max:
+                        pass
+                    else:
+                        x_mat[i,j] = x_mat[i-1,j]
+                elif j == 0:
+                    print("Bottom wall", i, " ",j)
+                    if 2 < x_mat[i,j] < 3:
+                        print("On the curve ",x_mat[i,j])
+                    else:
+                        print("Off the curve ", x_mat[i,j])
+                elif j == j_max:
+                    pass
+                else:
+                    i_coeff = i - 1
+                    j_coeff = j - 1
+                    print("inner node",i, "  ", j)
+                    
+        diff = 10**-10
+
+    pass
