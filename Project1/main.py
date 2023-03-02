63afb80463f52bbe51d6c17129a1784da79ee762
lcantugomez <luiscantugomez@outlook.com>
2023-03-01 21:41:36 -0600

    finished pde grid class and began debuggin pde solver

--- Project1/main.py
+++ Project1/main.py
@@ -2,10 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from algebraic_grid import create_alg_grid
-from pde_grid import coeff_calc, pde_grid
+from pde_grid import PDE_Grid
 
 y_lower = lambda x: np.piecewise(x,[x<=2,(x>2)*(x<3),x>=3],[lambda t:0, lambda t: (0.15*np.sin((t-2)*np.pi)), lambda t: 0])
 y_upper = lambda x: np.piecewise(x,[x<=2,(x>2)*(x<3),x>=3],[lambda t:1, lambda t: 1 - (0.15*np.sin((t-2)*np.pi)), lambda t: 1])
+yp_lower = lambda x: np.piecewise(x,[x<=2,(x>2)*(x<3),x>=3],[lambda t:0, lambda t: (0.15*np.pi*np.cos((t-2)*np.pi)), lambda t: 0])
+yp_upper = lambda x: np.piecewise(x,[x<=2,(x>2)*(x<3),x>=3],[lambda t:0, lambda t: -(0.15*np.pi*np.cos((t-2)*np.pi)), lambda t: 0])
+
 x_left = 0
 x_right = 5
 
@@ -15,22 +18,30 @@ j_max = 20
 d_exi = 1/i_max
 d_eta = 1/j_max
 
+
+
 x_pts,y_pts = create_alg_grid(i_max,j_max,x_left,x_right,y_lower,y_upper)
+
+params = {"i_max":i_max,"j_max":j_max,
+         "x_left":x_left,"x_right":x_right,
+         "y_lower":y_lower,"y_upper":y_upper,
+         "yp_lower":yp_lower,"yp_upper":yp_upper,
+         "x_mat":x_pts,"y_mat":y_pts,
+         "d_exi":d_exi,"d_eta":d_eta}
+
+
+grid_gen = PDE_Grid(params)
+
+grid_gen.coeff_calc()
+nx_pts,ny_pts = grid_gen.pde_grid(10**-7)
 
 x_arr = []
 y_arr = []
 
 for i in range(0,i_max + 1):
     for j in range(0, j_max + 1):
-        x_arr.append(x_pts[i,j])
-        y_arr.append(y_pts[i,j])
+        x_arr.append(nx_pts[i,j])
+        y_arr.append(ny_pts[i,j])
 
 plt.scatter(x_arr,y_arr)
 plt.show()
-
-"""print(x_pts)
-print(y_pts)"""
-
-alpha_mat, beta_mat, gamma_mat = coeff_calc(i_max,j_max,x_pts,y_pts,d_exi,d_eta)
-
-"""pde_grid(alpha_mat,beta_mat,gamma_mat,x_pts,y_pts,d_exi,d_eta,10**-7,i_max,j_max)"""
--- Project1/pde_grid.py
+++ Project1/pde_grid.py
@@ -2,62 +2,122 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def coeff_calc(i_max,j_max,x_mat,y_mat,d_exi,d_eta):
-    # Initialize alpha, beta, gamma arrays
+class PDE_Grid():
 
-    alpha_mat = np.zeros((i_max-1,j_max-1))
-    beta_mat = np.zeros((i_max-1,j_max-1))
-    gamma_mat = np.zeros((i_max-1,j_max-1))
+    # Init class with set of parameters
+    def __init__(self,params) -> None:      
+        for kw,arg in params.items():
+            setattr(PDE_Grid,kw,arg)
+    
 
-    for j in range(1,j_max):            # Loop only through the interior nodes
-        for i in range(1,i_max):
+
+    def coeff_calc(self):
+
+        i_max = self.i_max
+        j_max = self.j_max      # Init variables to use in function (is in this format due to legacy code)
+        x_mat = self.x_mat
+        y_mat = self.y_mat
+        d_exi = self.d_exi
+        d_eta = self.d_eta
+
+        # Initialize alpha, beta, gamma arrays
+
+        alpha_mat = np.zeros((i_max-1,j_max-1))
+        beta_mat = np.zeros((i_max-1,j_max-1))
+        gamma_mat = np.zeros((i_max-1,j_max-1))
+
+        for j in range(1,j_max):            # Loop only through the interior nodes
+            for i in range(1,i_max):
+                    
                 
 
-            alpha  = (1/(4*(d_eta**2))) * ( ((x_mat[i,j+1] - x_mat[i,j-1])**2) + ((y_mat[i,j+1] - y_mat[i,j-1]**2)) )
-            beta = (1/(4*(d_eta*d_exi))) * ( ((x_mat[i+1,j] - x_mat[i-1,j]) * (x_mat[i,j+1] - x_mat[i,j-1])) + ((y_mat[i+1,j] - y_mat[i-1,j]) * (y_mat[i,j+1] - y_mat[i,j-1])) )
-            gamma = (1/(4*(d_exi**2))) * ( ((x_mat[i+1,j] - x_mat[i-1,j])**2) + ((y_mat[i+1,j] - y_mat[i-1,j]**2)) )
+                # Calc the alpha beta and gamma vals
+
+                alpha  = (1/(4*(d_eta**2))) * ( ((x_mat[i,j+1] - x_mat[i,j-1])**2) + ((y_mat[i,j+1] - y_mat[i,j-1]**2)) )
+                beta = (1/(4*(d_eta*d_exi))) * ( ((x_mat[i+1,j] - x_mat[i-1,j]) * (x_mat[i,j+1] - x_mat[i,j-1])) + ((y_mat[i+1,j] - y_mat[i-1,j]) * (y_mat[i,j+1] - y_mat[i,j-1])) )
+                gamma = (1/(4*(d_exi**2))) * ( ((x_mat[i+1,j] - x_mat[i-1,j])**2) + ((y_mat[i+1,j] - y_mat[i-1,j]**2)) )
 
 
-            alpha_mat[i-1,j-1] = alpha
-            beta_mat[i-1,j-1] = beta
-            gamma_mat[i-1,j-1] = gamma
 
-    return alpha_mat, beta_mat, gamma_mat
+                # Adjust indices to use in matrix (python uses index 0, notes use index 1)
+
+                alpha_mat[i-1,j-1] = alpha
+                beta_mat[i-1,j-1] = beta
+                gamma_mat[i-1,j-1] = gamma
+
+        # Init mats into class
+
+        self.alpha_mat = alpha_mat
+        self.beta_mat = beta_mat
+        self.gamma_mat = gamma_mat
 
 
-def pde_grid(alpha_mat,beta_mat,gamma_mat,x_mat,y_mat,d_exi,d_eta,tol,i_max,j_max):
+        # Return variables for debugging
+        return alpha_mat, beta_mat, gamma_mat
 
-    diff = 1
-    while diff >= tol:
-        for j in range(0,j_max+1):
-            for i in range(0,i_max+1):
-                if i == 0:
-                    if j == 0:
-                        pass
-                    elif j == j_max:
-                        pass
-                    else:
-                        x_mat[i,j] = x_mat[i+1,j]
-                elif i == i_max:
-                    if j == 0:
-                        pass
-                    elif j == j_max:
-                        pass
-                    else:
-                        x_mat[i,j] = x_mat[i-1,j]
-                elif j == 0:
-                    print("Bottom wall", i, " ",j)
-                    if 2 < x_mat[i,j] < 3:
-                        print("On the curve ",x_mat[i,j])
-                    else:
-                        print("Off the curve ", x_mat[i,j])
-                elif j == j_max:
-                    pass
-                else:
-                    i_coeff = i - 1
-                    j_coeff = j - 1
-                    print("inner node",i, "  ", j)
+
+    def pde_grid(self,tol):
+
+        alpha_mat = self.alpha_mat
+        beta_mat = self.beta_mat
+        gamma_mat = self.gamma_mat
+        i_max = self.i_max
+        j_max = self.j_max              # Init variables to use in function (is in this format due to legacy code)
+        x_mat = self.x_mat
+        y_mat = self.y_mat
+        d_exi = self.d_exi
+        d_eta = self.d_eta
+
+        
+        diff = 1
+        while diff >= tol:
+            diff += -1
+            for j in range(0,j_max+1):
+                for i in range(0,i_max+1):
+
+                    if i == 0:
+                        if j == 0:
+                            pass            # Bottom left corner
+                        elif j == j_max:
+                            pass            # Top left corner
+                        else:
+                            x_mat[i,j] = x_mat[i+1,j]          # Left wall
+                            y_mat[i,j] = y_mat[i+1,j]
+
+                    elif i == i_max:
+                        if j == 0:
+                            pass            # Bottom right corner
+                        elif j == j_max:
+                            pass            # Top right corner
+                        else:
+                            x_mat[i,j] = x_mat[i-1,j]       # Right wall
+                            y_mat[i,j] = y_mat[i-1,j]
                     
-        diff = 10**-10
+                    elif j == 0:
+                        x_mat[i,j] = x_mat[i,j+2] * (2 * d_eta * self.__class__.yp_lower(x_mat[i,j]))     # On the bottom curve
+                        y_mat[i,j] = self.__class__.y_lower(x_mat[i,j-2])
+                        
+                    elif j == j_max:
+                        x_mat[i,j] = x_mat[i,j-2] * (2 * d_eta * self.__class__.yp_upper(x_mat[i,j]))     # On the upper curve
+                        y_mat[i,j] = self.__class__.y_upper(x_mat[i,j-2])
+                        
+                    else:
+                        i_coeff = i - 1
+                        j_coeff = j - 1
+                        
+                        phi = (2*self.alpha_mat[i_coeff,j_coeff]/(self.d_exi**2)) + (2*self.gamma_mat[i_coeff,j_coeff]/(self.d_eta**2))
 
-    pass
+                        a1 = -(self.beta_mat[i_coeff,j_coeff]/(2*self.d_exi*self.d_eta)) / phi
+                        a2 = -(self.gamma_mat[i_coeff,j_coeff]/(2*self.d_eta**2)) / phi
+                        a3 = -a1
+                        a4 = -(self.alpha_mat[i_coeff,j_coeff]/(2*self.d_exi**2)) / phi
+                        a5 = a4
+                        a6 = a3
+                        a7 = a2
+                        a8 = a1
+
+                        x_mat[i,j] = a1*x_mat[i-1,j] + a2*x_mat[i,j-1] + a3*x_mat[i+1,j-1] + a4*x_mat[i-1,j] + a5*x_mat[i+1,j] + a6*x_mat[i-1,j+1] + a7*x_mat[i,j+1] + a8*x_mat[i+1,j+1]
+
+                        y_mat[i,j] = a1*y_mat[i-1,j] + a2*y_mat[i,j-1] + a3*y_mat[i+1,j-1] + a4*y_mat[i-1,j] + a5*y_mat[i+1,j] + a6*y_mat[i-1,j+1] + a7*y_mat[i,j+1] + a8*y_mat[i+1,j+1]
+
+        return x_mat,y_mat
